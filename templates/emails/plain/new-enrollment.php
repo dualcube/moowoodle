dc1aadb843b03b2c30d8897cd50a648c6204e912
cool-raju <rajsekhar@dualcube.com>
2021-11-17 17:26:06 +0530

    Fixed multi time password send issue

--- templates/emails/plain/new-enrollment.php
+++ templates/emails/plain/new-enrollment.php
@@ -12,8 +12,11 @@ $enrollment_list = array();
 
 	$user_details = get_user_by( 'email', $user_data ); 
 	$pwd = get_user_meta( $user_details->data->ID , 'moowoodle_moodle_user_pwd', true );
-	echo esc_html( 'Username : ', 'moowoodle' ) . esc_html( $user_details->data->user_login ) . '\n\n';
-	echo esc_html( 'Password : ', 'moowoodle' ) .esc_html( $pwd ) . '\n\n';
+	$count_course_order = moodle_customer_created_orders_count($user_details->data->ID);
+	if ($count_course_order && $count_course_order < 2) {
+		echo esc_html( 'Username : ', 'moowoodle' ) . esc_html( $user_details->data->user_login ) . '\n\n';
+		echo esc_html( 'Password : ', 'moowoodle' ) .esc_html( $pwd ) . '\n\n';
+	}
 	echo esc_html( 'To enroll and access your course please click on the course link given below :', 'moowoodle' ) . '\n\n';
 
 foreach ( $enrollments[ 'enrolments' ] as $enrollment ) {
@@ -21,5 +24,6 @@ foreach ( $enrollments[ 'enrolments' ] as $enrollment ) {
 	echo esc_html( 'You are enrolled in '.$enrollment_list[ $i ] ).' \n\n';
 	$i++;
 }
-
-echo esc_html( 'You need to change your password after first login.', 'moowoodle' ) . '\n\n' ;
+if ($count_course_order && $count_course_order < 2) {
+	echo esc_html( 'You need to change your password after first login.', 'moowoodle' ) . '\n\n' ;
+}
