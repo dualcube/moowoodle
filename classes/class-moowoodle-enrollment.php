9fc5a52a8b8bf8634a00b0c7dc8dda1c88d873f8
kundan sow <kndnsow@gmail.com>
2024-03-05 21:59:37 +0530

    update 3.1.10

--- classes/class-moowoodle-enrollment.php
+++ classes/class-moowoodle-enrollment.php
@@ -42,8 +42,7 @@ class MooWoodle_Enrollment {
 		global $MooWoodle;
 		$wc_order = $this->wc_order;
 		$user_id = $wc_order->get_user_id();
-		do_action('moowoodle_get_moodle_user_id_before_enrollment', $user_id);
-		$moodle_user_id_meta = get_user_meta($user_id, 'moowoodle_moodle_user_id', true);
+		$moodle_user_id_meta =apply_filters('moowoodle_get_moodle_user_id_before_enrollment',get_user_meta($user_id, 'moowoodle_moodle_user_id', true), $user_id);
 		if (empty($moodle_user_id_meta)) {
 			//Metadata not found
 			$moodle_user_id_meta = $this->search_for_moodle_user('email', $wc_order->get_billing_email());
