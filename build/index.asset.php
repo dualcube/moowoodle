49fc2dbfb73b1149a2c0f581355dd91296bc80d6
Bishal221 <kayalbishal221@gmail.com>
2024-05-10 12:57:59 +0530

    Log Section css update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'c397def89a4c652690ea');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'e318a05fa82c3cd923d2');
