d5b85c49111455b17860f27b3c195fd812722f15
Bishal221 <kayalbishal221@gmail.com>
2024-05-16 18:42:20 +0530

    New commit

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '05b471f3bca2a4d24a37');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '75b4fe6ce55584fe9932');
