5710618f4f15ea10cafcbeb9f223cd36e145a479
moumitahalder <moumita.h@dualcube.com>
2024-05-16 17:55:39 +0530

    Test

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'ab4a2ac7ab8e8b48386b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '3d81d73a24c3cc6d2772');
--- src/template/settings/displaySettings.js
+++ src/template/settings/displaySettings.js
@@ -3,20 +3,20 @@ import { __ } from '@wordpress/i18n';
 export default {
     id: "display",
     priority: 20,
-    name: __("Course Information Display", 'moowoodle'),
-    desc: __("Manage learner access to course information", 'moowoodle'),
+    name: __("Shop Setup", 'moowoodle'),
+    desc: __("", 'moowoodle'),
     icon: "font-mail",
     submitUrl: "save-moowoodle-setting",
     modal: [
         {
             key: "start_end_date",
             type: "checkbox",
-            desc: __('If enabled display start date and end date in shop page.', 'moowoodle'),
-            label: __("Display Start Date and End Date in Shop Page", 'moowoodle'),
+            desc: __('If enabled display course duration in shop page.', 'moowoodle'),
+            label: __("Display course duration in Shop Page", 'moowoodle'),
             options: [
                 {
                     key: "start_end_date",
-                    label:  __('Enable', 'moowoodle'), 
+                    label:  __('', 'moowoodle'), 
                     value: "start_end_date"
                 }
             ]
@@ -24,8 +24,8 @@ export default {
         {
             key: "my_courses_priority",
             type: "select",
-            desc: __('Select below which menu the My Courses Menu will be displayed', 'moowoodle'),
-            label: __("My Courses Menu Position", 'moowoodle'),
+            desc: __('Select where the My Course menue will be displayed in My Account page.', 'moowoodle'),
+            label: __("Enpoint menu position - My Course", 'moowoodle'),
             options: Object.entries(appLocalizer.accountmenu).map(( [key, name ], index) => {
                 return {
                     key: index,
