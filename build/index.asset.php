be85629c6dfff8a867b0c7c1515aefa1dacf8f02
Pravat Sahoo <pravats459@gmail.com>
2024-05-23 11:24:56 +0530

    sync map fixed

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '48dfc1433531783c3e6c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '662975ebda8e0d58ea25');
