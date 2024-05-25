17c4fc25b4b1703df7c2348955b59fecc09adf8c
Bishal221 <kayalbishal221@gmail.com>
2024-05-25 16:24:53 +0530

    Build created

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '7622e3a8e314f4dc77a0');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '1e18a84d3ccb1aab5e2e');
