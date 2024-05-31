0aa3dbd8bfb0d5540db01c200eaca143a30cb404
moumitahalder <moumita.h@dualcube.com>
2024-05-31 14:02:21 +0530

    Text user 9

--- src/template/synchronizations/user.js
+++ src/template/synchronizations/user.js
@@ -10,18 +10,6 @@ export default {
     proDependent: true,
     modal: [
         {
-            key: 'separator_content',
-            type: 'section',
-            label: "",
-            desc: "Automatic, Real-time updates",
-            hint: "Under the automatic synchronization mode, user data is synchronized in real-time across WordPress and Moodle platforms in accordance with synchronization direction. <br>Whenever a new user is added or profile information is updated, it is synchronized, based on the specified direction and profile information mapping settings.<br>During user information synchronization, we verify the username linked to the email address. If we discover the same username in another instance but with a different email address, synchronization of the user's information is not possible."
-        },
-        {
-            key: 'separator_content',
-            type: 'section',
-            label: "",
-        },
-        {
             key: "user_sync_direction",
             type: "checkbox-custom-img",
             desc: __("Once enabled, the real-time profile update scheduler will initiate based on the synchronization direction you set between your WordPress and Moodle sites. <br>The user roles to be synchronized are defined by selecting the appropriate settings below.", 'moowoodle'),
@@ -67,11 +55,17 @@ export default {
             proSetting: true,
         },
         {
-            key: 'separator_content',
-            type: 'section',
-            label: "",
-            desc: "Manual mode",
-            hint: "In this mode, all current user accounts between WordPress and Moodle will be synchronized instantly according to the data synchronization direction configured above.<br>User uniqueness will be checked based on email. If the user exists in the other system, their profile information will be synchronized. If the user does not exist, a new user will be created."
+            key: "start_end_date",
+            type: "checkbox",
+            desc: __('When enabled, the course duration, such as the start and end dates, will be visible on the shop page.', 'moowoodle'),
+            label: __("Display course duration in Shop Page", 'moowoodle'),
+            options: [
+                {
+                    key: "start_end_date",
+                    label: __('', 'moowoodle'),
+                    value: "start_end_date"
+                }
+            ]
         },
         {
             key: "sync_user_btn",
