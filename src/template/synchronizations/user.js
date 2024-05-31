1deabf4690fbdf4f75f37cae4625f4155440cd1e
moumitahalder <moumita.h@dualcube.com>
2024-05-31 17:58:59 +0530

    Update user.js

--- src/template/synchronizations/user.js
+++ src/template/synchronizations/user.js
@@ -12,7 +12,7 @@ export default {
         {
             key: "user_sync_direction",
             type: "checkbox-custom-img",
-            desc: __("<b>The synchronization flow specifies the direction of data transfer. To enable two-way synchronization, select both directions. This applies to existing users as well. With 'Real-time profile synchronization,' user profile information will sync immediately whenever users update their profiles.</b><br> <span class='highlighted-part'>User uniqueness will be checked based on email. If the user exists in the other system, their profile information will be synchronized; otherwise, a new user will be created. <br>Synchronizing user information fails if the same username is found in another instance but linked to a different email address.</span>", 'moowoodle'),
+            desc: __("The synchronization flow specifies the direction of data transfer. To enable two-way synchronization, select both directions. This applies to existing users as well. With 'Real-time profile synchronization', user profile information will sync immediately whenever users update their profiles.<br><br> <span class='highlighted-part'>User uniqueness will be checked based on email. If the user exists in the other system, their profile information will be synchronized; otherwise, a new user will be created. <br>Synchronizing user information fails if the same username is found in another instance but linked to a different email address.</span>", 'moowoodle'),
             label: __("Synchronization flow between sites", 'moowoodle'),
             proSetting: true,
         },
@@ -57,7 +57,7 @@ export default {
         {
             key: "start_end_date",
             type: "checkbox",
-            desc: __('Once enabled, the real-time profile update scheduler will initiate based on the "synchronization flow" you set.<br>When a new user is added or updates their profile information, it will be synchronized between WordPress to Moodle, or vice versa, according to the profile information mapping settings below, based on the specified direction.', 'moowoodle'),
+            desc: __('If enabled, the real-time profile update scheduler will initiate based on the "synchronization flow" settings.<br>When a new user is added or updates their profile information, it will be synchronized between WordPress to Moodle, or vice versa, according to the profile information mapping settings above, based on the specified direction.', 'moowoodle'),
             label: __("Real-Time profile synchronization", 'moowoodle'),
             options: [
                 {
@@ -73,9 +73,9 @@ export default {
             interval: 10000,
             apilink: 'realtime-sync-users',
             statusApiLink: 'sync-status-user',
-            label: "Synchronize profile instantly",
-            value: "Update all current user profiles now!! ",
-            desc: __("Initiate the immediate synchronization of all users accounts between WordPress and Moodle according to the data synchronization flow configured above.<br><span class='highlighted-part'>During user information synchronization, we verify the username linked to the email address. If we discover the same username in another instance but with a different email address, synchronization of the user's information is not possible.</span>", 'moowoodle'),
+            label: "Sync profiles of existing users ",
+            value: "Synchronize users now!! ",
+            desc: __("This will trigger immediate synchronization of all existing user accounts between WordPress and Moodle based on the configured data synchronization flow.<br><br><span class='highlighted-part'>User uniqueness will be checked based on email. If the user exists in the other system, their profile information will be synchronized; otherwise, a new user will be created.Synchronizing user information fails if the same username is found in another instance but linked to a different email address.</span>", 'moowoodle'),
             proSetting: true,
         },
     ]
