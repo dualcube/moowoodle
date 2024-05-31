04822577c4a6af5d64e51f7c212dd5efb4fb961e
moumitahalder <moumita.h@dualcube.com>
2024-05-31 18:01:09 +0530

    Update user.js

--- src/template/synchronizations/user.js
+++ src/template/synchronizations/user.js
@@ -75,7 +75,7 @@ export default {
             statusApiLink: 'sync-status-user',
             label: "Sync profiles of existing users ",
             value: "Synchronize users now!! ",
-            desc: __("This will trigger immediate synchronization of all existing user accounts between WordPress and Moodle based on the configured data synchronization flow.<br><br><span class='highlighted-part'>User uniqueness will be checked based on email. If the user exists in the other system, their profile information will be synchronized; otherwise, a new user will be created.Synchronizing user information fails if the same username is found in another instance but linked to a different email address.</span>", 'moowoodle'),
+            desc: __("This will trigger immediate synchronization of all existing user accounts between WordPress and Moodle based on the configured data synchronization flow.<br><br><span class='highlighted-part'>User uniqueness will be checked based on email. If the user exists in the other system, their profile information will be synchronized; otherwise, a new user will be created.<br>Synchronizing user information fails if the same username is found in another instance but linked to a different email address.</span>", 'moowoodle'),
             proSetting: true,
         },
     ]
