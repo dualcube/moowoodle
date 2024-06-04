872e3a24191ef2b7afd83d739dc9fc8c6dce1450
Pravat Sahoo <pravats459@gmail.com>
2024-06-04 18:01:44 +0530

    build change

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '0aa8c4961febb461d989');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'd1c5058f4df33bb0928b');
