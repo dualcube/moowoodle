4184324a06d1f120fbf0b4258eea364f5dd673f5
Pravat Sahoo <pravats459@gmail.com>
2024-06-04 16:12:31 +0530

    error fixed

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'a45802dda92b1e9e8124');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '0aa8c4961febb461d989');
