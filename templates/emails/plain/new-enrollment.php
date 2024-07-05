0337e6db1ae90dc53210bd3a9ab85cafcebe7bb2
DAnn2012 <dannunziata@hotmail.it>
2024-07-05 23:40:54 +0200

    Update new-enrollment.php

--- templates/emails/plain/new-enrollment.php
+++ templates/emails/plain/new-enrollment.php
@@ -13,15 +13,15 @@ $user_details = get_user_by('email', $user_data);
 $pwd = get_user_meta($user_details->data->ID, 'moowoodle_moodle_user_pwd', true);
 $count_course_order = moodle_customer_created_orders_count($user_details->data->ID);
 if ($count_course_order && $count_course_order < 2) {
-	echo esc_html('Username : ', 'moowoodle') . esc_html($user_details->data->user_login) . '\n\n';
-	echo esc_html('Password : ', 'moowoodle') . esc_html($pwd) . '\n\n';
+	echo esc_html__('Username : ', 'moowoodle') . esc_html($user_details->data->user_login) . '\n\n';
+	echo esc_html__('Password : ', 'moowoodle') . esc_html($pwd) . '\n\n';
 }
-echo esc_html('To enroll and access your course please click on the course link given below :', 'moowoodle') . '\n\n';
+echo esc_html__('To enroll and access your course please click on the course link given below :', 'moowoodle') . '\n\n';
 foreach ($enrollments['enrolments'] as $enrollment) {
 	$enrollment_list[] = get_moowoodle_course_url($enrollment['courseid'], $enrollment['course_name']);
-	echo esc_html('You are enrolled in ' . $enrollment_list[$i]) . ' \n\n';
+	echo esc_html( __('You are enrolled in ', 'moowoodle') . $enrollment_list[$i] ) . ' \n\n';
 	$i++;
 }
 if ($count_course_order && $count_course_order < 2) {
-	echo esc_html('You need to change your password after first login.', 'moowoodle') . '\n\n';
+	echo esc_html__('You need to change your password after first login.', 'moowoodle') . '\n\n';
 }
