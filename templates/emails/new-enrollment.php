36c2639c19f44a547562594df37b81452ea0fcb9
DAnn2012 <dannunziata@hotmail.it>
2024-07-06 00:05:32 +0200

    Update new-enrollment.php

--- templates/emails/new-enrollment.php
+++ templates/emails/new-enrollment.php
@@ -17,8 +17,8 @@ $user_details = get_user_by('email', $user_data);
 $pwd = get_user_meta($user_details->data->ID, 'moowoodle_moodle_user_pwd', true);
 $count_course_order = moodle_customer_created_orders_count($user_details->data->ID);
 if ($count_course_order && $count_course_order < 2) {
-	echo esc_html__('Username : ', 'moowoodle') . esc_html__($user_details->data->user_login) . '<br><br>';
-	echo esc_html__('Password : ', 'moowoodle') . esc_html__($pwd) . '<br><br>';
+	echo esc_html__('Username : ', 'moowoodle') . esc_html($user_details->data->user_login) . '<br><br>';
+	echo esc_html__('Password : ', 'moowoodle') . esc_html($pwd) . '<br><br>';
 }
 echo esc_html__('To access your course please click on the course link given below :', 'moowoodle') . '<br><br>';
 ?>
