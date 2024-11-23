172012869aa48d16ff0cb4ed7e8c488ec5d6174f
Buddhadebroy <roybuddhadeb2003@gmail.com>
2024-11-23 18:45:24 +0530

    Css update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '4c1aa7e1e89793420146');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'e7a7cb246e7be6872c51');
