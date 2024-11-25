7139a25a809afc78e76285f96aafee47fa149ecd
Buddhadebroy <roybuddhadeb2003@gmail.com>
2024-11-25 12:46:25 +0530

    Final css

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'e7a7cb246e7be6872c51');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '2e74ef6961b43b642e3f');
