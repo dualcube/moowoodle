1581018403f51552c0a5be05d167014814a9c34a
Amit Sarkar <mroxygen2000@gmail.com>
2025-03-11 17:00:36 +0530

    minor

--- build/blocks/MyClassroom/index.asset.php
+++ build/blocks/MyClassroom/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '2fb03670450c11ecd5c4');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => 'b6a7c45d826b738b36b7');
--- src/blocks/MyClassroom/index.js
+++ src/blocks/MyClassroom/index.js
@@ -1,8 +1,9 @@
 import { render } from '@wordpress/element';
-import { BrowserRouter} from 'react-router-dom';
 import MyClassroom from './MyClassroom';
 
-
-
-// Render the App component into the DOM
-render(<BrowserRouter><MyClassroom /></BrowserRouter>, document.getElementById('moowoodle-my-classroom'));
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("moowoodle-my-classroom");
+    if (container) {
+        render(<MyClassroom />, container);
+    }
+});
