78516149196108653f8ac9e079e78e1bed59905a
Amit Sarkar <mroxygen2000@gmail.com>
2025-04-09 18:19:30 +0530

    fix error message

--- classes/Frontend.php
+++ classes/Frontend.php
@@ -4,15 +4,17 @@ namespace MooWoodle;
 class Frontend {
     public function __construct() {
         // Reset cart quantities after update
-        add_action('woocommerce_cart_updated', [$this, 'restrict_cart_quantity_on_update']);
+        add_action('woocommerce_cart_updated', [$this, 'enforce_quantity_restriction']);
+        // Add messages when cart/checkout is viewed
+        add_action('wp', [$this, 'show_cart_limit_notice']);
     }
 
     /**
-     * Ensure cart quantities stay at 1 after cart updates (server level)
+     * Enforce quantity restriction based on plugin version and settings
      */
-    public function restrict_cart_quantity_on_update() {
+    public function enforce_quantity_restriction() {
         $group_purchase_enabled = MooWoodle()->setting->get_setting('group_purchase_enable');
-        $is_khali_dabba = MooWoodle()->util->is_khali_dabba();
+        $is_khali_dabba = MooWoodle()->util->is_khali_dabba(); 
 
         if (
             is_array($group_purchase_enabled) &&
@@ -26,6 +28,30 @@ class Frontend {
             if ($cart_item['quantity'] > 1) {
                 WC()->cart->set_quantity($cart_item_key, 1);
             }
+        }
+    }
+
+    /**
+     * Show message on cart page based on version and settings
+     */
+    public function show_cart_limit_notice() {
+        if ( !is_cart() ) {
+            return;
+        }
+
+        $group_purchase_enabled = MooWoodle()->setting->get_setting('group_purchase_enable');
+        $is_khali_dabba = MooWoodle()->util->is_khali_dabba();
+
+        if (!$is_khali_dabba) {
+            wc_add_notice(
+                __('In the free version, each product is limited to a quantity of 1 per cart. Upgrade to Pro for unlimited quantities!', 'moowoodle'),
+                'notice'
+            );
+        } elseif (!is_array($group_purchase_enabled) || !in_array('group_purchase_enable', $group_purchase_enabled)) {
+            wc_add_notice(
+                __('Group purchase is disabled. Enable it in MooWoodle settings to allow multiple quantities.', 'moowoodle'),
+                'notice'
+            );
         }
     }
 }
