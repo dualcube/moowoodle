47ef274baa05fd7df2ddc3a79589b77567f4cd2b
Amit Sarkar <mroxygen2000@gmail.com>
2025-04-11 17:44:37 +0530

    minor

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'MOOWOODLE_PLUGIN_VERSION', '3.2.10' );
+define( 'MOOWOODLE_PLUGIN_VERSION', '3.2.11' );
 
 define( 'MOOWOODLE_PRO_SHOP_URL', 'https://dualcube.com/product/moowoodle-pro/' );
 define( 'MOOWOODLE_SUPPORT_URL', 'https://wordpress.org/support/plugin/moowoodle/' );
